2ffa5b345ca441ab5a5292232379758fc5a6a3b3
Tatiana Fofanova <tatiana.fofanova@gmail.com>
2022-03-13 16:07:09 +0300

    add trained model

--- train_model.py
+++ train_model.py
@@ -2,37 +2,20 @@ import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torchvision.io import read_image
+from mydataset import MyDataset
 import torchvision.transforms as T
+import torch.onnx
 import pickle
 import random
 import yaml
 import json
 from sklearn.metrics import accuracy_score
-import os
+import onnx
 
 
-class MyDataset(torch.utils.data.Dataset):
-    def __init__(self, X_data, Y_data, transform):
-        self.images = X_data
-        self.labels = Y_data
-        self.transform = transform
-
-    def __getitem__(self, idx):
-        img = np.expand_dims(self.images[idx], -1)
-        if self.transform is not None:
-            img = self.transform(img)
-
-        label = np.argmax(self.labels[idx])
-        return (img, label)
-
-    def __len__(self):
-        return len(self.images)
-
-
-class Model_cnn(nn.Module):
+class ModelCNN(nn.Module):
     def __init__(self):
-        super(Model_cnn, self).__init__()
+        super(ModelCNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(3, 3))
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(5, 5))
         self.fc1 = nn.Linear(29 * 29 * 16, 512)
@@ -56,9 +39,10 @@ def set_random_seed(seed):
     random.seed(seed)
 
 
-def train_one_epoch(model, train_dataloader, criterion, optimizer, device="cuda:0"):
+def train_one_epoch(model, dataloader, criterion, optimizer, device=torch.device("cuda:0")):
     model.train()
-    for x_batch, y_batch in train_dataloader:
+    loss = None
+    for x_batch, y_batch in dataloader:
         x_batch = x_batch.to(device)
         y_batch = y_batch.to(device)
         y_pred = model(x_batch)
@@ -71,7 +55,7 @@ def train_one_epoch(model, train_dataloader, criterion, optimizer, device="cuda:
     print(loss)
 
 
-def predict(model, dataloader, criterion, device="cuda:0"):
+def predict(model, dataloader, criterion, device=torch.device("cuda:0")):
     model.eval()
 
     # PREDICT FOR EVERY ELEMENT OF THE VAL DATALOADER AND RETURN CORRESPONDING LISTS
@@ -90,75 +74,74 @@ def predict(model, dataloader, criterion, device="cuda:0"):
     return losses, predicted_classes, true_classes
 
 
-def train(model, train_dataloader, val_dataloader, criterion, optimizer, device="cuda:0", n_epochs=10, scheduler=None):
+def train(model, train_dataloader, val_dataloader, criterion, optimizer, device=torch.device("cuda:0"),
+          epochs=10, scheduler=None):
     model.to(device)
-    for epoch in range(n_epochs):
+    for epoch in range(epochs):
         train_one_epoch(model, train_dataloader, criterion, optimizer, device)
-        losses, pred_classes, true_classes = predict(model, val_dataloader, criterion, device)
+        losses, predicted_classes, true_classes = predict(model, val_dataloader, criterion, device)
         scheduler.step(np.mean(losses))
 
-        accuracy = accuracy_score(pred_classes, true_classes)
+        accuracy = accuracy_score(predicted_classes, true_classes)
         print(f"{epoch}: {accuracy}")
 
 
-set_random_seed(120)
+if __name__ == "__main__":
+    set_random_seed(120)
 
-with open("datasets/x_train.pickle", "rb") as f:
-    X_train = pickle.load(f)
-with open("datasets/x_test.pickle", "rb") as f:
-    X_test = pickle.load(f)
-with open("datasets/y_train.pickle", "rb") as f:
-    Y_train = pickle.load(f)
-with open("datasets/y_test.pickle", "rb") as f:
-    Y_test = pickle.load(f)
+    with open("datasets/x_train.pickle", "rb") as f:
+        X_train = pickle.load(f)
+    with open("datasets/x_test.pickle", "rb") as f:
+        X_test = pickle.load(f)
+    with open("datasets/y_train.pickle", "rb") as f:
+        Y_train = pickle.load(f)
+    with open("datasets/y_test.pickle", "rb") as f:
+        Y_test = pickle.load(f)
 
-with open("params.yaml", "r") as f:
-    params = yaml.safe_load(f)["train_model"]
+    with open("params.yaml", "r") as f:
+        params = yaml.safe_load(f)["train_model"]
 
-# make dataloaders  
-train_transform = T.Compose([T.ToTensor(), T.Normalize((0.5), (0.5))])
-val_transform = T.Compose([T.ToTensor(), T.Normalize((0.5), (0.5))])
+    # make dataloaders
+    train_transform = T.Compose([T.ToTensor(), T.Normalize(0.5, 0.5)])
+    val_transform = T.Compose([T.ToTensor(), T.Normalize(0.5, 0.5)])
 
-train_dataset = MyDataset(X_train, Y_train, transform=train_transform)
-val_dataset = MyDataset(X_test, Y_test, transform=val_transform)
+    train_dataset = MyDataset(X_train, Y_train, transform=train_transform)
+    val_dataset = MyDataset(X_test, Y_test, transform=val_transform)
 
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=params["batch_size"], shuffle=True)
-val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=params["batch_size"])
+    my_train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=params["batch_size"], shuffle=True)
+    my_val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=params["batch_size"])
 
+    # train model
+    my_device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
-# train model
-device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    my_model = ModelCNN().to(my_device)
 
-model = Model_cnn().to(device)
+    my_optimizer = torch.optim.Adam(my_model.parameters(), lr=params["optimizer_lr"])
+    my_criterion = torch.nn.CrossEntropyLoss(reduction='mean')
+    my_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(my_optimizer, factor=params["scheduler_factor"],
+                                                              patience=params["scheduler_patience"], verbose=True)
+    n_epochs = params["n_epochs"]
 
-optimizer = torch.optim.Adam(model.parameters(), lr=params["optimizer_lr"])
-criterion = torch.nn.CrossEntropyLoss(reduction='mean')
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=params["scheduler_factor"],
-                                                       patience=params["scheduler_patience"], verbose=True)
-n_epochs = params["n_epochs"]
+    train(my_model, my_train_dataloader, my_val_dataloader, my_criterion,
+          my_optimizer, my_device, n_epochs, my_scheduler)
 
-train(model, train_dataloader, val_dataloader, criterion, optimizer, device, n_epochs, scheduler)
+    # write metrics
+    _, pred_classes, y_classes = predict(my_model, my_train_dataloader, my_criterion, my_device)
+    accuracy_train = accuracy_score(pred_classes, y_classes)
 
-# write model
-os.makedirs("models", exist_ok=True)
+    _, pred_classes, y_classes = predict(my_model, my_val_dataloader, my_criterion, my_device)
+    accuracy_val = accuracy_score(pred_classes, y_classes)
 
-with open("models/model.pickle", "wb") as f:
-    pickle.dump(model, f)
-
-# write metrics
-_, pred_classes, true_classes = predict(model, train_dataloader, criterion, device)
-accuracy_train = accuracy_score(pred_classes, true_classes)
-
-_, pred_classes, true_classes = predict(model, val_dataloader, criterion, device)
-accuracy_val = accuracy_score(pred_classes, true_classes)
-
-with open("metrics.json", "w") as f:
-    record = {
-        "train": {
-            "accuracy": accuracy_train
-        },
-        "valid": {
-            "accuracy": accuracy_val
+    with open("metrics.json", "w") as f:
+        record = {
+            "train": {
+                "accuracy": accuracy_train
+            },
+            "valid": {
+                "accuracy": accuracy_val
+            }
         }
-    }
-    json.dump(record, f)
+        json.dump(record, f)
+
+    # write model
+    torch.save(my_model.state_dict(), "models/model_pytorch")
